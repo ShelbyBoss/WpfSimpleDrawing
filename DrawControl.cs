c7d75c05e7d43978685791f16a7e520be2dd9728
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2018-09-04 22:38:03 +0200

    Initial commit

---
+++ DrawControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfSimpleDrawing
+{
+    class DrawControl : UserControl
+    {
+        public event EventHandler<DrawingContext> Paint;
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            Paint?.Invoke(this, drawingContext);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSimpleDrawing
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawControl_Paint(object sender, DrawingContext e)
+        {
+            DrawControl control = sender as DrawControl;
+
+            Pen pen = new Pen(Brushes.Green, 5);
+            Point point1 = new Point(0, 0);
+            Point point2 = new Point(control.ActualWidth, control.ActualHeight);
+
+            e.DrawLine(pen, point1, point2);
+        }
+
+        private void DrawControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
